c17b645d8109a2e16664f691a535c45331e4e5f4
Hal Mueller <halmueller@gmail.com>
2024-03-31 20:05:14 -0700

    checkpoint

---
+++ scripts/check_install.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+# URL = "https://example.com"
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+# print(page.text)
+
+soup = bs(page.content, "html.parser")
+#print(soup)
+
+results = soup.find(id="ResultsContainer")
+print(results.prettify)
